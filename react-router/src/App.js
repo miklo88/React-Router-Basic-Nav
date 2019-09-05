6dd93fcd81f61f4d34876626a32a2b9989b70e78
Carlos <miklo.concepcion@gmail.com>
2019-09-04 21:14:44 -0500

    Declaring routes and setting up Navigation. Finishing this.

---
+++ react-router/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import { Home, About, Contact, Navigation } from './components';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const App = () => (
+  
+    <div>
+     <Navigation />
+    </div>
+);
+
+export default App;
---
+++ react-router/src/components/Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+
+const Navigation = () => {
+  return (
+    <div>
+      <div className="App">
+        <h1>React Router Mini</h1>
+        <div className="nav-item">
+          <a className="Nav-link" href="">Home</a>
+        </div>
+        <div className="nav-item">
+          <a className="Nav-link" href="">About</a>
+        </div>
+        <div className="nav-item">
+          <a className="Nav-link" href="">Contact</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
